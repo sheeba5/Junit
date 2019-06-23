72d6132dcc6b9ba40d3b9a83625b15f982f1e017
Hemema Sheeba Methari <42418642+sheeba5@users.noreply.github.com>
2019-06-23 10:39:34 +0530

    Create checks.java

---
+++ checks.java
@@ -0,0 +1,26 @@
+package testing;
+
+public class Checking {
+
+	public String checks(String s) {
+		StringBuilder str=new StringBuilder(st);
+		if(s.charAt(0)=='A' && s.charAt(1)=='A')
+		{
+			str.delete(0,1);
+			
+		}
+		if(s.charAt(0)=='A')
+		{
+			str.deleteCharAt(0);
+		}
+		else {
+			if(s.charAt(1)=='A')
+			{
+				str.deleteCharAt(1);
+			}
+		}
+		return str.toString();
+	}
+	
+
+}
